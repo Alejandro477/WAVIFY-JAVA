04cd5ac156289afdd5a096a8b756f0cb8f1ede7c
guille152 <guillem.montero@stnicolau.com>
2026-04-16 12:02:23 +0200

    Commit 16/04 12:00
    Creada clase Reproductor
    Añadido:
    -Pasuar Cancion
    -Reproducir canción hacia delante
    -Reproducir hacia atrás canción
    -Parar cancion
    -Ajustar volumen

--- Wavify/src/main/java/Reproductor/Reproductor.java
+++ Wavify/src/main/java/Reproductor/Reproductor.java
@@ -4,6 +4,7 @@ package Reproductor;
 import Cancion.Cancion;
 import java.util.ArrayList;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 
 
@@ -50,8 +51,54 @@ public class Reproductor {
     public void setMediaplayer(MediaPlayer mediaplayer) {
         this.mediaplayer = mediaplayer;
     }
-
     
+    public void reproducir(){
+        if (this.mediaplayer!=null){
+            this.mediaplayer.play();
+            
+        }else{
+            System.out.println("error al reproducir");
+        }
+    }
+    
+    public void pausar(){
+          if (this.mediaplayer!=null){
+            this.mediaplayer.pause();
+          }
+    }
+    public void detener(){
+          if (this.mediaplayer!=null){
+            this.mediaplayer.stop();
+          }
+    }
+
+    public void adelantar(){
+        if (this.mediaplayer != null) {
+            Duration tiempoDestino = this.mediaplayer.getCurrentTime().add(Duration.seconds(10));
+            this.mediaplayer.seek(tiempoDestino);
+    }
+    }
+    public void atrasar(){
+        if (this.mediaplayer != null) {
+            Duration tiempoDestino = this.mediaplayer.getCurrentTime().subtract(Duration.seconds(10));
+            this.mediaplayer.seek(tiempoDestino);
+    }
+    
+        
+    }
+    public void ajustarVolumen(int nivel){
+        if (this.mediaplayer != null) {
+            if (nivel < 0) nivel = 0;
+            if (nivel > 100) nivel = 100;
+            
+            double volumennormalizado = nivel / 100.0;
+            this.mediaplayer.setVolume(volumennormalizado);
+       
+    }
+      
+            
+    }
 }
+    
 
 
